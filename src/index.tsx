c6fbe61f36dff289c53d290e7e727185a30d7071
MarinaSenina <frau.senina32@yandex.ru>
2023-04-09 12:27:20 +0300

    feat: i18n

--- src/index.tsx
+++ src/index.tsx
@@ -3,7 +3,7 @@ import App from "./app/App";
 import {BrowserRouter} from "react-router-dom";
 import React from "react";
 import {ThemeProvider} from "app/providers/ThemeProvider";
-
+import "shared/config/i18n/i18n";
 
 render(
     <BrowserRouter>
--- src/widgets/Sidebar/ui/Sidebar/Sidebar.tsx
+++ src/widgets/Sidebar/ui/Sidebar/Sidebar.tsx
@@ -2,6 +2,8 @@ import {classNames} from "shared/lib/classNames";
 import cls from "./Sidebar.module.scss"
 import React, {useState} from "react";
 import {ThemeSwitcher} from "widgets/ThemeSwitcher";
+import {LineFeed} from "ts-loader/dist/constants";
+import {LangSwitcher} from "widgets/LangSwitcher/LangSwitcher";
 
 
 interface SidebarProps {
@@ -22,6 +24,7 @@ export const Sidebar = ({className}: SidebarProps) => {
         <button onClick={onToggle}>toggle</button>
             <div className={cls.switchers}>
                 <ThemeSwitcher />
+                <LangSwitcher />
             </div>
         </div>
     );
