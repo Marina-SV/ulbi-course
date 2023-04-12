e3ee229699800c923c578cbc14512a0f7d2fba58
MarinaSenina <frau.senina32@yandex.ru>
2023-04-12 15:37:12 +0300

    feat: add stylelint, i18n plugin for eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -6,6 +6,7 @@ module.exports = {
     extends: [
         'plugin:react/recommended',
         'airbnb',
+        'plugin:i18next/recommended',
     ],
     parser: '@typescript-eslint/parser',
     parserOptions: {
@@ -18,7 +19,9 @@ module.exports = {
 
     plugins: [
         'react',
+        'i18next',
     ],
+
     rules: {
         'react/jsx-indent': [2, 4],
         'react/jsx-indent-props': [2, 4],
@@ -35,6 +38,7 @@ module.exports = {
         'import/extensions': 'off',
         'import/no-extraneous-dependencies': 'off',
         'no-underscore-dangle': 'off',
+        'i18next/no-literal-string': ['error', { markupOnly: true }],
     },
     globals: {
         __IS_DEV__: true,
